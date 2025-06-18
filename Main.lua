a469a61ba94617751b574ea2212c85673607af1a
Tuubby <mmantas.kazlauskas@gmail.com>
2025-06-18 11:23:18 +0300

    Init commit

---
+++ Main.lua
@@ -0,0 +1,9 @@
+---@class ChattyLittleNpc_The_Burning_Crusade_voiceovers
+local ChattyLittleNpc_The_Burning_Crusade_voiceovers = LibStub("AceAddon-3.0"):NewAddon("ChattyLittleNpc_The_Burning_Crusade_voiceovers")
+
+ChattyLittleNpc_The_Burning_Crusade_voiceovers.Metadata = {
+    ContainsQuestVoiceovers = true,
+    ContainsGossipVoiceovers = false,
+    ContainsGameObjectVoiceovers = false,
+    ContainsItemVoiceovers = false
+}
